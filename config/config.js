7a234778f270888b35fbd1c0793e46c2c478e784
Dennis Onder <webdev.dennisonder@gmail.com>
2019-06-30 14:13:43 +0200

    Ditched the web service for a SPA

--- config/config.js
+++ config/config.js
@@ -4,7 +4,6 @@ module.exports = {
   SECRET_OR_KEY: process.env.SECRET_OR_KEY,
   AUTH_SERVER_PORT: process.env.AUTH_SERVER_PORT,
   BOARD_SERVER_PORT: process.env.BOARD_SERVER_PORT,
-  WEB_SERVER_PORT: process.env.WEB_SERVER_PORT,
   AUTH_DB_URI: process.env.AUTH_DB_URI,
   BOARD_DB_URI: process.env.BOARD_DB_URI,
   API_VERSION: process.env.API_VERSION
