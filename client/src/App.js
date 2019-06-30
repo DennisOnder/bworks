73d33eb258ea8afe14a41e4a329b38451d7e9160
Dennis Onder <webdev.dennisonder@gmail.com>
2019-06-30 14:21:20 +0200

    Client-side prepwork

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Base.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>bworks</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
