df84063736cb7d3412b800d09f324c1adffdfe6c
Dennis Onder <webdev.dennisonder@gmail.com>
2019-07-05 23:00:55 +0200

    Redirect from landing if logged in

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,28 +1,34 @@
 import React from "react";
 import AuthDrawer from "./imports/AuthDrawer";
+import { Redirect } from "react-router-dom";
+import isLoggedIn from "../functions/isLoggedIn";
 
 const openDrawer = e =>
   (document.getElementById(`auth__drawer__${e.target.name}`).style.display =
     "flex");
 
 const Landing = () => {
-  return (
-    <div className="landing">
-      <header className="landing-header">
-        <p>bworks</p>
-        <div>
-          <button name="register" onClick={openDrawer}>
-            Register
-          </button>
-          <button name="login" onClick={openDrawer}>
-            Log In
-          </button>
-        </div>
-        <AuthDrawer type="login" />
-        <AuthDrawer type="register" />
-      </header>
-    </div>
-  );
+  if (isLoggedIn()) {
+    return <Redirect to="/dashboard" />;
+  } else {
+    return (
+      <div className="landing">
+        <header className="landing-header">
+          <p>bworks</p>
+          <div>
+            <button name="register" onClick={openDrawer}>
+              Register
+            </button>
+            <button name="login" onClick={openDrawer}>
+              Log In
+            </button>
+          </div>
+          <AuthDrawer type="login" />
+          <AuthDrawer type="register" />
+        </header>
+      </div>
+    );
+  }
 };
 
 export default Landing;
